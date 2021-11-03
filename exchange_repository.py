f0c77b918372f36a0b9d3e65a71c9c172037b9f2
just4faan <vteleleyko@gmail.com>
2021-11-03 09:56:44 +0200

    create and insert data in excahnge_repository

--- exchange_repository.py
+++ exchange_repository.py
@@ -7,5 +7,12 @@ class ExchangeRepo():
     def __init__(self):
         self.curr_db = CurrencyDB()
         self.ws = WebService()
-        self.kek = 'real kek'
-        print(self.kek)
+
+        # self.curr_db.json_latest = self.ws.get_latest_json_currencies()
+        # self.curr_db.create()
+        self.curr_db.read()
+        # self.curr_db.drop_latest_table()
+
+
+if __name__ == '__main__':
+    pass
